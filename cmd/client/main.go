3720e7ec9a72f80ef89ce86cb5c0048085499c18
Daren McCulley <mcculleydj@gmail.com>
2021-02-24 16:33:36 -0500

    go client and server implementation

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,5 +1,100 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+	"time"
 
+	"github.com/mcculleydj/chat/pkg/proto"
+
+	"google.golang.org/grpc"
+)
+
+func listen(stream proto.Chat_JoinClient, errChan chan error) {
+	for {
+		msg, err := stream.Recv()
+		if err == io.EOF {
+			log.Println("connection terminated by server")
+			errChan <- err
+			return
+		}
+		if err != nil {
+			// TODO: look into grpc status lib for better error handling
+			if strings.Contains(err.Error(), "username already registered") {
+				errChan <- errors.New("username already registered")
+				return
+			}
+			log.Println("error receiving msg:", err.Error())
+			errChan <- err
+			return
+		}
+		fmt.Printf("%s | %s - %s\n", msg.User.Name, msg.Timestamp, msg.Content)
+	}
+}
+
+func talk(client proto.ChatClient, user *proto.User, errChan chan error) {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		if scanner.Text() == "quit" {
+			_, err := client.Leave(context.Background(), user)
+			if err != nil {
+				errChan <- err
+				return
+			}
+			errChan <- errors.New("")
+			return
+		}
+
+		_, err := client.Broadcast(context.Background(), &proto.Message{
+			User:      user,
+			Content:   scanner.Text(),
+			Timestamp: time.Now().Format("15:04:05"),
+		})
+		if err != nil {
+			log.Println("error sending msg:", err.Error())
+			errChan <- err
+		}
+	}
+}
+
+func main() {
+	name := flag.String("n", "", "username")
+	flag.Parse()
+	if *name == "" {
+		log.Fatal("must supply a username with the -n flag")
+	}
+
+	// WithBlock blocks until the underlying connection is up.
+	// Otherwise Dial returns immediately and connecting to the server happens in background.
+	conn, err := grpc.Dial("localhost:3000", grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer conn.Close()
+
+	client := proto.NewChatClient(conn)
+	user := &proto.User{Name: *name}
+	errChan := make(chan error)
+
+	stream, err := client.Join(context.Background(), user)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	go listen(stream, errChan)
+	go talk(client, user, errChan)
+
+	err = <-errChan
+	if err.Error() == "" {
+		fmt.Println("Leaving chat...")
+	} else {
+		fmt.Println(err.Error())
+	}
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,105 @@
 package main
 
-func main() {
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"sync"
+	"time"
 
+	"github.com/mcculleydj/chat/pkg/proto"
+	"google.golang.org/grpc"
+)
+
+type connection struct {
+	stream  proto.Chat_JoinServer
+	errChan chan error
+}
+
+type chatServer struct {
+	connections map[string]*connection
+	sync.Mutex
+	proto.UnimplementedChatServer
+}
+
+var bot *proto.User
+
+func init() {
+	bot = &proto.User{Name: "ChatBot"}
+}
+
+func (s *chatServer) Join(u *proto.User, stream proto.Chat_JoinServer) error {
+	errChan := make(chan error)
+	// way to use defer and still perform bot broadcast safely here?
+	s.Lock()
+	if _, ok := s.connections[u.Name]; ok {
+		s.Unlock()
+		return errors.New("username already registered")
+	}
+	s.connections[u.Name] = &connection{stream, errChan}
+	s.Unlock()
+	// TODO: what to do with context?
+
+	// TODO: what to do if this fails?
+	s.Broadcast(context.Background(), &proto.Message{
+		User:      bot,
+		Content:   fmt.Sprintf("%s has joined...", u.Name),
+		Timestamp: time.Now().Format("15:04:05"),
+	})
+
+	// will block until there is an error calling stream.Send()
+	return <-errChan
+}
+
+func (s *chatServer) Broadcast(ctx context.Context, msg *proto.Message) (*proto.Ack, error) {
+	// TODO: what to do with context?
+
+	s.Lock()
+	defer s.Unlock()
+	for name, conn := range s.connections {
+		go func(thisName string, thisConn *connection) {
+			if thisName == msg.User.Name {
+				return
+			}
+			err := thisConn.stream.Send(msg)
+			if err != nil {
+				fmt.Printf("error sending to %s: %v\n", thisName, err)
+				thisConn.errChan <- err
+			}
+		}(name, conn)
+	}
+
+	// TODO: should this function return an error if .Send() generates an error
+	// if it does not how does a client know that this message did not reach everyone?
+
+	return &proto.Ack{}, nil
+}
+
+func (s *chatServer) Leave(ctx context.Context, u *proto.User) (*proto.Ack, error) {
+	s.Lock()
+	delete(s.connections, u.Name)
+	s.Unlock()
+
+	return s.Broadcast(context.Background(), &proto.Message{
+		User:      bot,
+		Content:   fmt.Sprintf("%s has left...", u.Name),
+		Timestamp: time.Now().Format("15:04:05"),
+	})
+}
+
+func main() {
+	lis, err := net.Listen("tcp", "localhost:3000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	// TODO: is NewServer() insecure by default? need to pass this in Node
+	grpcServer := grpc.NewServer()
+	server := &chatServer{connections: make(map[string]*connection)}
+	proto.RegisterChatServer(grpcServer, server)
+
+	// .Serve() is blocking until .Stop() or .GracefulStop() is called
+	// .Serve() also returns an error
+	grpcServer.Serve(lis)
 }
